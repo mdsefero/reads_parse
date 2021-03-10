33ec0593bcd9b9d9f5ed88a25e9c57fee66b7e04
Maxim Seferovic <60980841+mdsefero@users.noreply.github.com>
2021-03-09 19:00:05 -0600

    Update Reform_header.py

--- Reform_header.py
+++ Reform_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 ####################################################################
-#Reformats older illumina HiSeq fastqs to newer format
-#Usage: Reform_header.py -f [infilenames] OPTIONS
+#Reformats older Illumina fastqs to newer format
+#Usage: Reform_header.py -f [infilenames] Options
 #Mandatory argument -f --file, infiles
 #Optional argument -p --processes, (default autodetct)
-#Processing may be I/O limited on standard machines
-#4 March 2021
+#Processing may be I/O limited
+#9 March 2021
 #Maxim Seferovic, seferovi@bcm.edu
 ####################################################################
 
@@ -16,37 +16,41 @@ from datetime import datetime
 def timestamp(action, object):
     print(
         f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S') : <22}"
-        f"{action : <12}"
+        f"{action : <18}"
         f"{object}"
         )
-    
+
 def filecheck (fname):
     if os.path.isfile(fname.rsplit('.', 1)[0] + '_fmt.' + fname.rsplit('.', 1)[-1]): 
         print ("Processed file exists, skipping\t", fname)
     else: return fname
-      
+
 def load_mod (n_processors, file):
     timestamp('Loading:', file)
     with open (file, 'r') as fp:
-        line_chunks = split(fp.readlines(), n_processors)
+        n, line_chunks = split(fp.readlines(), n_processors)
     sname = file.rsplit('.', 1)[0] + '_fmt.' + file.rsplit('.', 1)[-1]
-       
-    timestamp('Processing:', file)
-    return_dict = manager.dict()  
+    timestamp('Lines processing:', n)
+    return_dict = manager.dict()
     new_line_chunks = []
     for count, i in enumerate(line_chunks):
         p = multiprocessing.Process(target=lprocess, args=(i, count, return_dict))
         new_line_chunks.append(p)
         p.start()
     for i in new_line_chunks: i.join()
-        
+
     timestamp('Saving:', sname)
+    #{k: d[k] for k in sorted(d)}
+    count = 0
     with open (sname, 'w') as f:
-        for i in return_dict.keys(): f.write("".join(return_dict[i]))
+        for i in sorted(return_dict.keys()): 
+            f.write("".join(return_dict[i]))
+            count = count + len(return_dict[i])
+    timestamp('Lines saved:', count)
 
 def split(a, n):  # function to split a list in n even parts
     k, m = divmod(len(a), n)
-    return list((a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)))
+    return len(a), list((a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)))
 
 def lprocess(lines, count, return_dict):
     new_lines = []
@@ -66,7 +70,7 @@ def main (f,p):
     for i, file in enumerate(file_list): 
         load_mod(p, file)
         if i % 10 == 0: print(f"File proccessing ~{int(i/len(file_list)*100)}% finished...")
-            
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f',  '--file', nargs ='+', required=True, type=str, dest='in_file')
